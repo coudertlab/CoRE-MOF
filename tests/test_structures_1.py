f019b3f81b263e13547506ad79f51fbd84569143
Francois-Xavier Coudert <fxcoudert@gmail.com>
2024-03-27 00:13:55 +0100

    Improve tests

--- tests/test_structures_1.py
+++ tests/test_structures_1.py
@@ -5,12 +5,14 @@ import pytest
 @pytest.mark.filterwarnings("ignore:.*rounded to ideal values")
 def test_structures_1():
     import CoRE_MOF
+    from pymatgen.core import Structure
 
     x = CoRE_MOF.list_structures("2019-ASR")
     assert "1499489-acs.cgd.6b01265_1499490_clean" in x
     assert "ZUZZEB_clean" in x
 
     s = CoRE_MOF.get_structure("2019-ASR", "1499489-acs.cgd.6b01265_1499490_clean")
+    assert isinstance(s, Structure)
     a, b, c = s.lattice.abc
     assert math.isclose(a, 18.975)
     assert math.isclose(b, 18.975)
@@ -22,6 +24,7 @@ def test_structures_1():
     assert s.num_sites == 756
 
     s = CoRE_MOF.get_structure("2019-ASR", "ZUZZEB_clean")
+    assert isinstance(s, Structure)
     a, b, c = s.lattice.abc
     assert math.isclose(a, 29.311)
     assert math.isclose(b, 29.311)
